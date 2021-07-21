ec5c60a93130e30088c87131607da961be8c57e5
Lauren Kessell <lkessell1@gmail.com>
2021-07-21 13:05:31 -0400

    Add test for main view with visible cards

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -10,4 +10,10 @@ describe('Dashboard user flows', () => {
     cy.visit('http://localhost:3000/');
   });
 
+  it('Should be able to visit http://localhost:3000/ and see a list of reservations', () => {
+    cy.get('.app-title').contains('Turing Cafe Reservations');
+    cy.get('.reserv-card').contains('Christie');
+    cy.get('.reserv-card').contains('Robbie');
+  });
+
 });
