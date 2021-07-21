eb9ba21c977eda3b1104bce339b10436c055c3d3
Lauren Kessell <lkessell1@gmail.com>
2021-07-21 12:22:04 -0400

    Pass data through props

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -3,7 +3,16 @@ import React from 'react';
 import ReservCard from '../ReservCard/ReservCard';
 
 const Reservations = ({ reservations }) => {
-  const reservCards = reservations.map(reservation => <ReservCard />);
+  const reservCards = reservations.map(reservation => {
+    return (
+      <ReservCard
+        key={reservation.id}
+        id={reservation.id}
+        name={reservation.name}
+        date={reservation.date}
+        time={reservation.time}
+        number={reservation.number}
+      />)});
 
   return (
     <section className='reserv-container'>
