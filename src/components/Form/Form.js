d2e97a7b1c9b3dd7518478242309cd1560fbd1a3
Lauren Kessell <lkessell1@gmail.com>
2021-07-21 12:32:17 -0400

    Set up base component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  render() {
+    return (
+      <h2>I'm a form</h2>
+    )
+  }
+}
+
+export default Form;
