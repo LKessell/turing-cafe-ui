508acb9531c64f563e1820cd40e246a70ea4b9d7
Lauren Kessell <lkessell1@gmail.com>
2021-07-21 12:41:36 -0400

    Add form inputs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,12 +8,37 @@ class Form extends Component {
       name: '',
       date: '',
       time: '',
-      number: 0
+      number: ''
     }
   }
   render() {
     return (
       <form>
+        <input
+          type='text'
+          name='name'
+          value={this.state.name}
+          placeholder='Name'
+        />
+        <input
+          type='text'
+          name='date'
+          value={this.state.date}
+          placeholder='Date (MM/DD)'
+        />
+        <input
+          type='text'
+          name='time'
+          value={this.state.time}
+          placeholder='Time'
+        />
+        <input
+          type='text'
+          name='number'
+          value={this.state.number}
+          placeholder='Number of Guests'
+        />
+        <button>Make Reservation</button>
       </form>
     )
   }
